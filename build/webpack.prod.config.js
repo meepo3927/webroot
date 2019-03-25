56ee9a43af960762f31309dfb84bfae63e315a0d
guoye <guoye@nmcc.nrc>
2019-03-25 17:58:03 +0800

    cache yeah

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -24,6 +24,7 @@ const rules = [
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
+            cacheDirectory: true,
             presets: [["env", {
               "modules": false,
               "targets": {
