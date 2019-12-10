3c4a89f348cbe30b61cb2f23e5c01869d3c84924
guoye <meepo3927@163.com>
2019-12-10 17:52:47 +0800

    amap

--- js/comp/amap.vue
+++ js/comp/amap.vue
@@ -5,84 +5,6 @@
 <script>
 const MAP_SRC = 'https://webapi.amap.com/maps?v=1.4.15&key=54fc25c73b5aa3a4d5fef63d7095f19e&callback=onAMapLoad';
 const methods = {};
-// 添加标记
-methods.addMarker = function (item) {
-    const lng = item.lng;
-    const lat = item.lat;
-    if (!item || !item.lng || !item.lat) {
-        return null;
-    }
-    const marker = new AMap.Marker({
-        map: this.map,
-        position: new AMap.LngLat(item.lng, item.lat),
-        extData: item,
-        label: {
-            content: item.markerContent,
-            direction: 'bottom'
-        }
-    });
-    marker.on('click', (e) => {
-        this.$emit('marker-click', marker, this, e);
-    });
-    const name = item.markerId || Tool.randStr();
-    this.markerHolder[name] = marker;
-    return marker;
-};
-methods.cleanMarker = function () {
-    for (let k in this.markerHolder) {
-        if (this.markerHolder.hasOwnProperty(k)) {
-            this.map.remove(this.markerHolder[k]);
-        }
-    }
-    this.markerHolder = {};
-};
-// 多边形
-methods.addPolygon = function (item, options = {}) {
-    const points = item.points;
-    if (!points || !points.length) {
-        return null;
-    }
-    const polygonPath = points.map(v => {
-        return new AMap.LngLat(v.lng, v.lat)
-    });
-    // 中心点
-    // const centerPoint = this.getPolygonCenter(points);
-    const o = Tool.extend({
-        map: this.map,
-        strokeWeight: 2,
-        strokeColor: '#1472CE',
-        strokeStyle: 'dashed',
-        fillColor: '#1472CE',
-        fillOpacity: .5,
-        cursor: 'pointer'
-    }, options);
-    o.extData = item;
-    o.path = polygonPath;
-    const polygon = new AMap.Polygon(o);
-    polygon.on('click', (e) => {
-        this.$emit('polygon-click', e, this, this.map);
-    });
-    const name = item.polygonId || Tool.randStr();
-    this.polygonHolder[name] = polygon;
-    return polygon;
-};
-methods.getPolygonCenter = function (list) {
-    let x = 0.0;
-    let y = 0.0;
-    for (let i = 0; i < list.length; i++) {
-        x += parseFloat(list[i].lng);
-        y += parseFloat(list[i].lat);
-    }
-    x /= list.length;
-    y /= list.length;
-    return new AMap.LngLat(x, y);
-};
-methods.getPolygonById = function (id) {
-    return this.polygonHolder[id];
-};
-methods.getMarkerById = function (id) {
-    return this.markerHolder[id];
-};
 methods.loadMap = function (callback) {
     if (window.AMap) {
         callback && callback(window.AMap);
@@ -133,6 +55,93 @@ methods._ = function (callback) {
     } else {
         this.initStack.push(callback);
     }
+};
+// 添加工具条
+methods.addToolbar = function (options) {
+    const o = Tool.extend({
+        position: 'LB'
+    }, options);
+    AMap.plugin(['AMap.ToolBar'], () => {
+        this.map.addControl(new AMap.ToolBar(o));
+    });
+};
+// 添加标记
+methods.addMarker = function (item) {
+    const lng = item.lng;
+    const lat = item.lat;
+    if (!item || !item.lng || !item.lat) {
+        return null;
+    }
+    const marker = new AMap.Marker({
+        map: this.map,
+        position: new AMap.LngLat(item.lng, item.lat),
+        extData: item,
+        label: {
+            content: item.markerContent,
+            direction: 'bottom'
+        }
+    });
+    marker.on('click', (e) => {
+        this.$emit('marker-click', marker, this, e);
+    });
+    const name = item.markerId || Tool.randStr();
+    this.markerHolder[name] = marker;
+    return marker;
+};
+methods.cleanMarker = function () {
+    for (let k in this.markerHolder) {
+        if (this.markerHolder.hasOwnProperty(k)) {
+            this.map.remove(this.markerHolder[k]);
+        }
+    }
+    this.markerHolder = {};
+};
+methods.getMarkerById = function (id) {
+    return this.markerHolder[id];
+};
+// 多边形
+methods.addPolygon = function (item, options = {}) {
+    const points = item.points;
+    if (!points || !points.length) {
+        return null;
+    }
+    const polygonPath = points.map(v => {
+        return new AMap.LngLat(v.lng, v.lat)
+    });
+    // 中心点
+    // const centerPoint = this.getPolygonCenter(points);
+    const o = Tool.extend({
+        map: this.map,
+        strokeWeight: 2,
+        strokeColor: '#1472CE',
+        strokeStyle: 'dashed',
+        fillColor: '#1472CE',
+        fillOpacity: .5,
+        cursor: 'pointer'
+    }, options);
+    o.extData = item;
+    o.path = polygonPath;
+    const polygon = new AMap.Polygon(o);
+    polygon.on('click', (e) => {
+        this.$emit('polygon-click', e, this, this.map);
+    });
+    const name = item.polygonId || Tool.randStr();
+    this.polygonHolder[name] = polygon;
+    return polygon;
+};
+methods.getPolygonById = function (id) {
+    return this.polygonHolder[id];
+};
+methods.getPolygonCenter = function (list) {
+    let x = 0.0;
+    let y = 0.0;
+    for (let i = 0; i < list.length; i++) {
+        x += parseFloat(list[i].lng);
+        y += parseFloat(list[i].lat);
+    }
+    x /= list.length;
+    y /= list.length;
+    return new AMap.LngLat(x, y);
 };
 const computed = {};
 const created = function () {};
